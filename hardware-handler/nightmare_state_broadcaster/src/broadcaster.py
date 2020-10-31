5eb877d0615aade39d0b28d93a1755e799d6e1cc
nikisalli <niki.salli@gmail.com>
2020-10-31 20:06:18 +0100

    added basic state publisher

--- hardware-handler/nightmare_state_broadcaster/src/broadcaster.py
+++ hardware-handler/nightmare_state_broadcaster/src/broadcaster.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
+
 import rospy
+import numpy as np
+import time
+from std_msgs.msg import Byte
+
+state = 0
+
+def handle_state():
+    rate = rospy.Rate(50)
+    pub_state = rospy.Publisher("nightmare/state", Byte, queue_size=1)
+    
+    while True:
+        pub_state.publish(state)
+        rate.sleep()
+    
+if __name__ == '__main__':
+    rospy.init_node('state_broadcaster')
+    
+    rospy.loginfo("starting state broadcaster")
+    handle_state()
