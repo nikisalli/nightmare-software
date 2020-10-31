46089dfe247717513146bbe94e9b2804168b5cac
nikisalli <niki.salli@gmail.com>
2020-10-31 18:58:11 +0100

    added setup scripts

--- hardware-handler/nightmare_movement_engine/setup.py
+++ hardware-handler/nightmare_movement_engine/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['nightmare_hardware_handler'],
+    packages=['nightmare_movement_engine'],
     package_dir={'': 'src'},
 )
 
--- hardware-handler/nightmare_state_broadcaster/setup.py
+++ hardware-handler/nightmare_state_broadcaster/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['nightmare_hardware_handler'],
+    packages=['nightmare_state_broadcaster'],
     package_dir={'': 'src'},
 )
 
