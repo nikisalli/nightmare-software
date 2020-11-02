8e328238dba975b36e70041800b541653d4cf6b0
nikisalli <niki.salli@gmail.com>
2020-11-02 17:05:11 +0100

    added first engine structure

--- hardware-handler/nightmare_movement_engine/src/engine.py
+++ hardware-handler/nightmare_movement_engine/src/engine.py
@@ -1,2 +1,30 @@
-import rospy
+#!/usr/bin/env python3
 
+# ros imports
+import rospy
+from std_msgs.msg import Byte
+
+class engine_node():
+    def __init__(self):
+        self.state = 0
+        
+    def run(self):
+        while not rospy.is_shutdown():
+            pass
+    
+    def set_state(self, msg):
+        self.state = msg.data
+
+
+if __name__ == '__main__':
+    rospy.init_node('movement_engine')
+    
+    rospy.loginfo("starting engine node")
+
+    engine = engine_node()
+    
+    rospy.loginfo("subscribing to state_broadcaster node")
+    
+    rospy.Subscriber("/nightmare/state", Byte, engine.set_state)
+    
+    engine.run()
--- hardware-handler/nightmare_state_broadcaster/src/broadcaster.py
+++ hardware-handler/nightmare_state_broadcaster/src/broadcaster.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import rospy
+# standard python imports
 import numpy as np
 import time
+
+# ros imports
+import rospy
 from std_msgs.msg import Byte
 
 state = 0
 
 def handle_state():
     rate = rospy.Rate(50)
-    pub_state = rospy.Publisher("nightmare/state", Byte, queue_size=1)
+    pub_state = rospy.Publisher("/nightmare/state", Byte, queue_size=1)
     
-    while True:
+    while not rospy.is_shutdown():
         pub_state.publish(state)
         rate.sleep()
     
