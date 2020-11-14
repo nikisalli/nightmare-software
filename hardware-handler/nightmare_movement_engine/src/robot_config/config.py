387187a6924d3c528b48fe8e7dfaf19695744270
nikisalli <niki.salli@gmail.com>
2020-11-14 13:58:01 +0100

    fixed config

--- hardware-handler/nightmare_movement_engine/src/robot_config/config.py
+++ hardware-handler/nightmare_movement_engine/src/robot_config/config.py
@@ -1,17 +1,17 @@
 import numpy as np
 from dataclasses import dataclass
 
-STAND_HEIGTH = -16
-STAND_MID_LEG_Y = 26
-STAND_OUT_LEG_Y = 20
-STAND_MID_LEG_X = 0
-STAND_OUT_LEG_X = 20
+STAND_HEIGTH = -16.
+STAND_MID_LEG_Y = 26.
+STAND_OUT_LEG_Y = 20.
+STAND_MID_LEG_X = 0.
+STAND_OUT_LEG_X = 20.
 BODY_LENGTH = 15.5  # X
 BODY_MID_WIDTH = 18.6  # Y
 BODY_OUT_WIDTH = 13.7  # Y
 LEG_COXA_LENGTH = 6.5
-LEG_FEMUR_LENGTH = 13
-LEG_TIBIA_LENGTH = 17
+LEG_FEMUR_LENGTH = 13.
+LEG_TIBIA_LENGTH = 17.
 RIGHT = True
 LEFT = False
 PI = np.pi
@@ -37,7 +37,7 @@ legs = [
         abs_offset=np.array([BODY_LENGTH/2, -BODY_OUT_WIDTH/2, 0]),
         default_pose=np.array([STAND_OUT_LEG_X, -STAND_OUT_LEG_Y, STAND_HEIGTH]),
         side=RIGHT,
-        servo_offset=np.array([-PI/4, 0, -PI/2])
+        servo_offset=np.array([PI/4, 0, -PI/2])
     ),
     leg_class(  # leg 2
         dim=DEFAULT_DIM,
@@ -51,14 +51,14 @@ legs = [
         abs_offset=np.array([-BODY_LENGTH/2, -BODY_OUT_WIDTH/2, 0]),
         default_pose=np.array([-STAND_OUT_LEG_X, -STAND_OUT_LEG_Y, STAND_HEIGTH]),
         side=RIGHT,
-        servo_offset=np.array([PI/4, 0, -PI/2])
+        servo_offset=np.array([-PI/4, 0, -PI/2])
     ),
     leg_class(  # leg 4
         dim=DEFAULT_DIM,
         abs_offset=np.array([-BODY_LENGTH/2, BODY_OUT_WIDTH/2, 0]),
         default_pose=np.array([-STAND_OUT_LEG_X, STAND_OUT_LEG_Y, STAND_HEIGTH]),
         side=LEFT,
-        servo_offset=np.array([-PI/4, 0, -PI/2])
+        servo_offset=np.array([PI/4, 0, -PI/2])
     ),
     leg_class(  # leg 5
         dim=DEFAULT_DIM,
@@ -72,8 +72,9 @@ legs = [
         abs_offset=np.array([BODY_LENGTH/2, BODY_OUT_WIDTH/2, 0]),
         default_pose=np.array([STAND_OUT_LEG_X, STAND_OUT_LEG_Y, STAND_HEIGTH]),
         side=LEFT,
-        servo_offset=np.array([PI/4, 0, -PI/2])
+        servo_offset=np.array([-PI/4, 0, -PI/2])
     )
 ]
 
-DEFAULT_POSE = [leg.default_pose for leg in legs]
+DEFAULT_POSE = np.array([leg.default_pose for leg in legs])
+SERVO_OFFSET = np.append(np.array([leg.servo_offset for leg in legs]).ravel(), 0)
