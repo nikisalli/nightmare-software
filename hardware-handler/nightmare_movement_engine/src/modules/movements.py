ac69a28d1c36d28da94674110354e846c0bf1935
nikisalli <niki.salli@gmail.com>
2020-11-15 21:53:11 +0100

    fixed base_link has inertia: inertia is no more

--- hardware-handler/nightmare_movement_engine/src/modules/movements.py
+++ hardware-handler/nightmare_movement_engine/src/modules/movements.py
@@ -7,4 +7,5 @@ from modules.config import DEFAULT_POSE
 
 def sleep(engine):
     engine.pose = DEFAULT_POSE.copy()
+    engine.pose[:, 2] += sin(time.time()*2)*10
     engine.angles_array = abs_pos2ang(engine.pose)
