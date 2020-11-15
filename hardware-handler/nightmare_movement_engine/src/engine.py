8a4188d1cbe19d7ab88b784566ab7a612d07f6c9
nikisalli <niki.salli@gmail.com>
2020-11-15 17:49:17 +0100

    non va un cazzo

--- hardware-handler/nightmare_movement_engine/src/engine.py
+++ hardware-handler/nightmare_movement_engine/src/engine.py
@@ -64,7 +64,8 @@ class engineNode():
     def set_hw_joint_state(self, msg):
         self.hw_angles_array = msg.position
         self.hw_angles = np.reshape(self.hw_angles_array[:-1], newshape=(6, 3))
-        # self.hw_pose = abs_ang2pos(self.hw_angles_array)
+        self.hw_pose = abs_ang2pos(self.hw_angles_array)
+        print(self.hw_pose)
 
 
 if __name__ == '__main__':
