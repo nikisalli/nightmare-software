a2c01caddad87bdb152c4602d4bea8163bac50df
nikisalli <niki.salli@gmail.com>
2020-11-16 22:46:42 +0100

    fixed cmakelist and setup.py

--- hardware-handler/nightmare_control/setup.py
+++ hardware-handler/nightmare_control/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['nightmare_movement_engine'],
+    packages=['nightmare_control'],
     package_dir={'': 'src'},
 )
 
