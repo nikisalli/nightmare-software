fixed nasty rotation matrix bug

nightmare-software/nightmare_movement_engine/src/main.py
@@ -58,7 +58,8 @@ class engineNode():
         self.pose = DEFAULT_POSE.copy()  # pose of the robot without transforms
         self.final_pose = DEFAULT_POSE.copy()  # pose of the robot with transforms
         self.rate = rospy.Rate(ENGINE_FPS)
-        self.body_pos = np.array([0, 0, 0])
+        self.body_trasl = [0, 0, 0]
+        self.body_rot = [0, 0, 0]
 
         # step planner state
         self.step_id = 0

nightmare-software/nightmare_movement_engine/src/modules/movements.py
@@ -1,6 +1,8 @@
 from random import randint
 import time
 import numpy as np
+from numpy import sin, cos
+
 import rospy
 
 import tf_conversions
@@ -48,8 +50,12 @@ def step(engine):
 
     starttime = time.time()
 
+    divider = 0
+
     if engine.gait == 'tripod':
-        duration = STEP_TIME / (ENGINE_FPS * 2)
+        divider = 2
+
+    duration = 1 / (ENGINE_FPS * STEP_TIME * divider)
 
     for i in range(int(STEP_TIME * ENGINE_FPS)):
         for leg in range(NUMBER_OF_LEGS):
@@ -57,8 +63,13 @@ def step(engine):
                 engine.pose[leg] = np.array(curves[leg].evaluate(i / (STEP_TIME * ENGINE_FPS))).flatten()
             else:
                 new_pose = engine.pose[leg].copy() + (np.asarray([engine.walk_direction[1], - engine.walk_direction[0], 0.0]) / - (STEP_TIME * ENGINE_FPS))
-                new_pose = euler_rotation_matrix(new_pose, [0, 0, engine.walk_direction[2] / - (STEP_TIME * ENGINE_FPS)])
+                new_pose = euler_rotation_matrix(new_pose, [0, 0, engine.walk_direction[2] / (STEP_TIME * ENGINE_FPS)])
                 engine.pose[leg] = new_pose
+
+        # generate odom
+        engine.body_rot[2] += - engine.walk_direction[2] / (STEP_TIME * ENGINE_FPS)
+        engine.body_trasl[0] += - (engine.walk_direction[1] / (STEP_TIME * ENGINE_FPS)) * sin(engine.body_rot[2]) + (engine.walk_direction[0] / (STEP_TIME * ENGINE_FPS)) * cos(engine.body_rot[2])
+        engine.body_trasl[1] += (engine.walk_direction[1] / (STEP_TIME * ENGINE_FPS)) * cos(engine.body_rot[2]) + (engine.walk_direction[0] / (STEP_TIME * ENGINE_FPS)) * sin(engine.body_rot[2])
 
         apply_transform(engine)
 
@@ -136,11 +147,11 @@ def apply_transform(engine):
     engine.final_pose = pose
 
     # generate odom
-    engine.engine_pos_publisher_msg.transform.translation.x = engine.body_displacement[0]
-    engine.engine_pos_publisher_msg.transform.translation.y = - engine.body_displacement[1]
+    engine.engine_pos_publisher_msg.transform.translation.x = engine.body_displacement[0] + engine.body_trasl[0]
+    engine.engine_pos_publisher_msg.transform.translation.y = - engine.body_displacement[1] + engine.body_trasl[1]
     engine.engine_pos_publisher_msg.transform.translation.z = - STAND_HEIGTH - engine.body_displacement[2]
     # convert rpy to ros friendly quaternion
-    q = tf_conversions.transformations.quaternion_from_euler(engine.body_displacement[3], - engine.body_displacement[4], 0)
+    q = tf_conversions.transformations.quaternion_from_euler(engine.body_displacement[3], - engine.body_displacement[4], engine.body_rot[2])
     engine.engine_pos_publisher_msg.transform.rotation.x = q[0]
     engine.engine_pos_publisher_msg.transform.rotation.y = q[1]
     engine.engine_pos_publisher_msg.transform.rotation.z = q[2]
@@ -152,11 +163,11 @@ def sleep(engine):
     engine.engine_pos_publisher_msg.transform.translation.z = - SIT_HEIGHT
 
     # generate odom
-    engine.engine_pos_publisher_msg.transform.translation.x = engine.body_displacement[0]
-    engine.engine_pos_publisher_msg.transform.translation.y = - engine.body_displacement[1]
-    engine.engine_pos_publisher_msg.transform.translation.z = - engine.body_displacement[2]
+    engine.engine_pos_publisher_msg.transform.translation.x = engine.body_trasl[0]
+    engine.engine_pos_publisher_msg.transform.translation.y = engine.body_trasl[1]
+    engine.engine_pos_publisher_msg.transform.translation.z = engine.body_trasl[2]
     # convert rpy to ros friendly quaternion
-    q = tf_conversions.transformations.quaternion_from_euler(engine.body_displacement[3], - engine.body_displacement[4], 0)
+    q = tf_conversions.transformations.quaternion_from_euler(0, 0, engine.body_rot[2])
     engine.engine_pos_publisher_msg.transform.rotation.x = q[0]
     engine.engine_pos_publisher_msg.transform.rotation.y = q[1]
     engine.engine_pos_publisher_msg.transform.rotation.z = q[2]

nightmare-software/nightmare_step_planner/src/main.py
@@ -3,6 +3,7 @@
 import json
 import numpy as np
 from numpy import sqrt
+from scipy.spatial.transform import Rotation as R
 
 # ros imports
 import rospy
@@ -11,7 +12,7 @@ from nightmare_step_planner.msg import command  # pylint: disable=no-name-in-mod
 from std_msgs.msg import Header, Int32, String
 from visualization_msgs.msg import Marker
 
-from nightmare_math.math import euler_rotation_matrix, quaternion_transform
+from nightmare_math.math import euler_rotation_matrix
 from nightmare_config.config import GAIT_TRIPOD, LEG_TIPS, DEFAULT_POSE, PI
 
 
@@ -78,21 +79,19 @@ class stepPlannerNode():
         header = Header()
 
         while not rospy.is_shutdown():
-            if self.parse_tf():
-                dpose = euler_rotation_matrix(DEFAULT_POSE, [0, 0, PI / 2])
-                new_pose = quaternion_transform(dpose, self.body_trans, rotation=False)
-                self.publish_pose_markers(new_pose, 1, self.green)
+            self.parse_tf()
 
             mod = sqrt(self.walk_direction[0]**2 + self.walk_direction[1]**2)
+
             if (self.engine_step >= self.step_id - 1 and self.state == 'stand' and (abs(mod) > 0.01 or abs(self.walk_direction[2]) > 0.01)):
                 # check if:
                 # - a step is already present
                 # - check if the robot is standing to start walking
                 # - check if a command to walk is valid
 
-                rospy.loginfo(f"planner_step: {self.step_id} planner_gait_step: {self.gait_step} engine_step: {self.engine_step}")
-
                 self.generate_next_steps()
+
+                rospy.loginfo(f"planner_step: {self.step_id} planner_gait_step: {self.gait_step} engine_step: {self.engine_step}")
 
                 header.stamp = rospy.Time.now()
                 self.pub_steps.publish(String(json.dumps(self.steps)))
@@ -105,15 +104,20 @@ class stepPlannerNode():
             step = []
 
             dpose = euler_rotation_matrix(DEFAULT_POSE, [0, 0, PI / 2])
-            abs_target_pose = quaternion_transform(dpose, self.body_trans, rotation=False)
+            translation = [self.body_trans.translation.x, self.body_trans.translation.y, self.body_trans.translation.z]
+            rotation = [self.body_trans.rotation.x, self.body_trans.rotation.y, self.body_trans.rotation.z, self.body_trans.rotation.w]
+            rotation_matrix = R.from_quat(rotation)
+            abs_target_pose = rotation_matrix.apply(dpose)
+            abs_target_pose += translation
+
+            self.publish_pose_markers(abs_target_pose, 1, self.green)
 
             for leg in GAIT_TRIPOD[self.gait_step]:
                 prev_pos = abs_target_pose[leg]
 
                 new_pos = np.array([prev_pos[0] + self.walk_direction[0], prev_pos[1] + self.walk_direction[1], 0.0])
-                new_pos = euler_rotation_matrix(new_pos, [0, 0, self.walk_direction[2]])
 
-                trans = euler_rotation_matrix(new_pos - prev_pos, [0, 0, - PI / 2])
+                trans = euler_rotation_matrix(new_pos - prev_pos, [0, 0, self.walk_direction[2]])
 
                 step.append({'leg': int(leg), 'pos': trans.tolist()})
 
@@ -140,11 +144,11 @@ class stepPlannerNode():
         try:
             # get leg tip absolute transform
             for i, leg in enumerate(LEG_TIPS):
-                trans = self.tf_buffer.lookup_transform('world', leg, rospy.Time.now(), rospy.Duration(3.0)).transform.translation
+                trans = self.tf_buffer.lookup_transform('world', leg, rospy.Time.now(), rospy.Duration(.1)).transform.translation
                 self.abs_body_pose[i] = [trans.x, trans.y, trans.z]
 
             # get body transform
-            trans = self.tf_buffer.lookup_transform('world', 'body_link', rospy.Time.now(), rospy.Duration(3.0)).transform
+            trans = self.tf_buffer.lookup_transform('world', 'body_link', rospy.Time.now(), rospy.Duration(.1)).transform
             self.body_trans = trans
 
             return 1