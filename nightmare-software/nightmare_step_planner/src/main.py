dfb0d034511c14196fdd34be0f8ed7e1c81377f2
nikisalli <niki.salli@gmail.com>
2021-01-17 00:42:08 +0100

    step planner 100x faster (wrong tf request time)

--- nightmare-software/nightmare_step_planner/src/main.py
+++ nightmare-software/nightmare_step_planner/src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import time
 import numpy as np
 from numpy import sqrt
 from scipy.spatial.transform import Rotation as R
@@ -13,7 +14,7 @@ from std_msgs.msg import Header, Int32, String, Float32
 from visualization_msgs.msg import Marker
 
 from nightmare_math.math import euler_rotation_matrix
-from nightmare_config.config import GAIT_TRIPOD, LEG_TIPS, DEFAULT_POSE, PI, MAX_STEP_LENGTH
+from nightmare_config.config import GAIT, LEG_TIPS, DEFAULT_POSE, PI, MAX_STEP_LENGTH
 
 
 class stepPlannerNode():
@@ -24,7 +25,6 @@ class stepPlannerNode():
         self.prev_state = 'sleep'  # previous engine state
         self.body_displacement = [0] * 6
         self.walk_direction = [0] * 3
-        self.walk_direction_derivative = [0] * 3
 
         # step planner state
         self.steps = []
@@ -74,95 +74,92 @@ class stepPlannerNode():
         self.blue = [0, 0, 1]
 
         # publishers
+        self.header = Header()
         self.pub_steps = rospy.Publisher("/engine/footsteps", String, queue_size=1)
         self.marker_publisher = rospy.Publisher("/engine/markers", Marker, queue_size=100)
         self.engine_attenuation_publisher = rospy.Publisher('/engine/attenuation', Float32, queue_size=10)
         self.engine_attenuation_msg = Float32()  # engine current step id
 
     def run(self):
-        header = Header()
-
         while not rospy.is_shutdown():
             self.parse_tf()
-
             mod = sqrt(self.walk_direction[0]**2 + self.walk_direction[1]**2)
-
-            if (self.engine_step >= self.step_id - 1 and self.state == 'stand' and (abs(mod) > 0.01 or abs(self.walk_direction[2]) > 0.01)):
+            if (self.engine_step >= self.step_id - (len(GAIT[self.gait]) / 2) and self.state == 'stand' and (abs(mod) > 0.01 or abs(self.walk_direction[2]) > 0.01)):
                 # check if:
                 # - a step is already present
                 # - check if the robot is standing to start walking
                 # - check if a command to walk is valid
-
                 self.generate_next_steps()
-
-                rospy.loginfo(f"planner_step: {self.step_id} planner_gait_step: {self.gait_step} engine_step: {self.engine_step}")
-
-                header.stamp = rospy.Time.now()
-                self.pub_steps.publish(String(json.dumps(self.steps)))
-
             self.rate.sleep()
 
     def generate_next_steps(self):
+        t = time.time()
+
         self.step_id += 1
-        if self.gait == 'tripod':
-            step = []
+        step = []
 
-            dpose = euler_rotation_matrix(DEFAULT_POSE, [0, 0, PI / 2])
-            translation = [self.body_trans.translation.x, self.body_trans.translation.y, self.body_trans.translation.z]
-            rotation = [self.body_trans.rotation.x, self.body_trans.rotation.y, self.body_trans.rotation.z, self.body_trans.rotation.w]
-            rotation_matrix = R.from_quat(rotation)
-            rotation_euler = rotation_matrix.as_euler('xyz')
-            inverse_rotation_matrix = rotation_matrix.inv()
-            abs_target_pose = rotation_matrix.apply(dpose)
-            abs_target_pose += translation
+        dpose = euler_rotation_matrix(DEFAULT_POSE, [0, 0, PI / 2])
+        translation = [self.body_trans.translation.x, self.body_trans.translation.y, self.body_trans.translation.z]
+        rotation = [self.body_trans.rotation.x, self.body_trans.rotation.y, self.body_trans.rotation.z, self.body_trans.rotation.w]
+        rotation_matrix = R.from_quat(rotation)
+        rotation_euler = rotation_matrix.as_euler('xyz')
+        inverse_rotation_matrix = rotation_matrix.inv()
+        abs_target_pose = rotation_matrix.apply(dpose)
+        abs_target_pose += translation
 
-            self.publish_pose_markers(abs_target_pose, 1, self.green)
+        self.publish_pose_markers(abs_target_pose, 1, self.green)
 
-            # find max displacement
-            npose = abs_target_pose.copy()
-            npose += euler_rotation_matrix([self.walk_direction[0], self.walk_direction[1], 0], [0, 0, rotation_euler[2]])
-            npose -= translation
-            npose = euler_rotation_matrix(npose, [0, 0, - self.walk_direction[2]])
-            npose = inverse_rotation_matrix.apply(npose)
-            tr = np.subtract(npose, dpose)
-            displ = max([sqrt(leg[0]**2 + leg[1]**2 + leg[2]**2) for leg in tr])
+        # find absolute max displacement
+        npose = abs_target_pose.copy()
+        npose += euler_rotation_matrix([self.walk_direction[0] * 0.5, self.walk_direction[1] * 0.5, 0], [0, 0, rotation_euler[2]])
+        npose -= translation
+        npose = euler_rotation_matrix(npose, [0, 0, - self.walk_direction[2] * 0.5])
+        npose = inverse_rotation_matrix.apply(npose)
+        tr = np.subtract(npose, dpose)
+        displ = max([sqrt(leg[0]**2 + leg[1]**2 + leg[2]**2) for leg in tr])
 
-            if displ > MAX_STEP_LENGTH:
-                self.attenuation = MAX_STEP_LENGTH / displ
-            else:
-                self.attenuation = 1
+        # generate a coefficient to scale the step so that is stays in the defined limits
+        if displ > MAX_STEP_LENGTH:
+            self.attenuation = MAX_STEP_LENGTH / displ
+        else:
+            self.attenuation = 1
 
-            # generate steps
-            for leg in GAIT_TRIPOD[self.gait_step]:
-                prev_pos = abs_target_pose[leg]
-                new_pos = prev_pos.copy()
+        # generate steps
+        for leg in GAIT[self.gait][self.gait_step]:
+            prev_pos = abs_target_pose[leg]
+            new_pos = prev_pos.copy()
 
-                #  =======================
-                # rotate command with the body or you end up with messed matrices!
-                new_pos += euler_rotation_matrix([self.walk_direction[0] * self.attenuation, self.walk_direction[1] * self.attenuation, 0], [0, 0, rotation_euler[2]])
+            #  =======================
+            # rotate command with the body or you end up with messed matrices!
+            comtrans = euler_rotation_matrix([self.walk_direction[0] * self.attenuation * 0.5, self.walk_direction[1] * self.attenuation * 0.5, 0], [0, 0, rotation_euler[2]])
+            new_pos += comtrans
+            abs_trans = comtrans + translation
 
-                # can't be bothered TODO
-                new_pos -= translation
-                new_pos = euler_rotation_matrix(new_pos, [0, 0, - self.walk_direction[2] * self.attenuation])
-                new_pos += translation
-                #  =======================
+            # can't be bothered TODO
+            new_pos -= translation
+            new_pos = euler_rotation_matrix(new_pos, [0, 0, - self.walk_direction[2] * self.attenuation * 0.5])
+            new_pos += translation
+            #  =======================
 
-                self.publish_marker(new_pos, 1, self.red)
+            self.publish_marker(new_pos, 1, self.red)
 
-                new_pos -= translation
-                new_pos = inverse_rotation_matrix.apply(new_pos)
-                trans = new_pos - dpose[leg]
+            new_pos -= translation
+            new_pos = inverse_rotation_matrix.apply(new_pos)
+            trans = new_pos - dpose[leg]
 
-                step.append({'leg': int(leg), 'pos': trans.tolist()})
+            step.append({'leg': int(leg), 'pos': trans.tolist()})
 
-            self.steps.append({'id': int(self.step_id), 'steps': step})
+        self.steps.append({'id': int(self.step_id), 'trans': abs_trans.tolist(), 'steps': step})
+        self.gait_step += 1
+        self.publish_attenuation()
 
-            self.gait_step += 1
+        if self.gait_step == len(GAIT[self.gait]):  # check if gait cycle got to the end
+            self.gait_step = 0
 
-            self.publish_attenuation()
+        self.header.stamp = rospy.Time.now()
+        self.pub_steps.publish(String(json.dumps(self.steps)))
 
-            if self.gait_step == len(GAIT_TRIPOD):  # check if gait cycle got to the end
-                self.gait_step = 0
+        rospy.loginfo(f"generation took: {time.time() - t}s planner_step: {self.step_id} planner_gait_step: {self.gait_step} engine_step: {self.engine_step}")
 
     def set_state(self, msg):
         self.state = msg.state
@@ -178,11 +175,11 @@ class stepPlannerNode():
         try:
             # get leg tip absolute transform
             for i, leg in enumerate(LEG_TIPS):
-                trans = self.tf_buffer.lookup_transform('world', leg, rospy.Time.now(), rospy.Duration(.1)).transform.translation
+                trans = self.tf_buffer.lookup_transform('world', leg, rospy.Time(0), rospy.Duration(.1)).transform.translation
                 self.abs_body_pose[i] = [trans.x, trans.y, trans.z]
 
             # get body transform
-            trans = self.tf_buffer.lookup_transform('world', 'body_link', rospy.Time.now(), rospy.Duration(.1)).transform
+            trans = self.tf_buffer.lookup_transform('world', 'body_link', rospy.Time(0), rospy.Duration(.1)).transform
             self.body_trans = trans
 
             return 1
