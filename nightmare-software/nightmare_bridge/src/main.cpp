b21cd851950d3b1b8e18e26a5d53a6bbf3ef612c
nikisalli <niki.salli@gmail.com>
2021-04-20 02:13:23 +0200

    fixed deps and started bridge

--- nightmare-software/nightmare_bridge/src/main.cpp
+++ nightmare-software/nightmare_bridge/src/main.cpp
@@ -1,22 +1,30 @@
- /*
-  * Hello World Example using ROS and CPP
-  */
- 
- // Include the ROS library
- #include <ros/ros.h>
- 
- // Main function
- int main(int argc, char** argv)
- { 
-   // Initialize the ROS Node "roscpp_hello_world"
-   ros::init(argc, argv, "roscpp_hello_world");
- 
-   // Instantiate the ROS Node Handler as nh
-   ros::NodeHandle nh;
-          
-   // Print "Hello ROS!" to the terminal and ROS log file
-   ROS_INFO_STREAM("Hello from ROS node " << ros::this_node::getName());
-          
-   // Program succesful
-   return 0;
- } 
+#include <ros/ros.h>
+#include <octomap/octomap.h>
+#include <octomap/ColorOcTree.h>
+#include <octomap/AbstractOcTree.h>
+#include <octomap_msgs/conversions.h>
+#include <octomap_msgs/Octomap.h>
+
+void Callback(const octomap_msgs::OctomapConstPtr& msg){
+    octomap::ColorOcTree* map = NULL;
+    octomap::AbstractOcTree* tree = octomap_msgs::msgToMap(*msg);
+    if (tree){
+        map = dynamic_cast<octomap::ColorOcTree*>(tree);
+        if(!map){
+            ROS_ERROR("Wrong octomap type. Use a different display type.");
+        }
+    } else {
+        ROS_ERROR( "Failed to deserialize octree message.");
+        return;
+    }
+    ROS_INFO("leaves: %d", map->getNumLeafNodes());
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "bridge");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("/rtabmap/octomap_binary", 1000, Callback);
+    ros::spin();
+
+    return 0;
+}
