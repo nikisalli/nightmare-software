acfe05b64077011fb07278be7b16be60b9a5b2e9
nikisalli <niki.salli@gmail.com>
2022-01-24 23:32:09 +0100

    added fixed tip joint transforms

--- nightmare-software/nightmare/src/nightmare/engine.py
+++ nightmare-software/nightmare/src/nightmare/engine.py
@@ -7,14 +7,15 @@ import rospy
 
 # module imports
 from nightmare.modules.logging import printlog, loglevel, pinfo, pwarn, perr, pfatal
+from nightmare.config import *
 
 
 class engine_node:
     def __init__(self):
         # hardware data
-        self.local_pos = np.zeros(shape=(3, 6))  # position of each leg starting from the attachment to the body
-        self.body_pos = np.zeros(shape=(3, 6))  # position of each leg with the center of the body as origin
-        self.ground_pos = np.zeros(shape=(3, 6))  # body pos but with rotation relative to the ground
+        self.local_pos = np.zeros(shape=(3, 6))  # hardware position of each leg starting from the attachment to the body
+        self.body_pos = np.zeros(shape=(3, 6))  # hardware position of each leg with the center of the body as origin
+        self.ground_pos = np.zeros(shape=(3, 6))  # hardware body pos but with rotation relative to the ground
         pinfo("ready")
 
     def compute_ik(self, ground_pos):
